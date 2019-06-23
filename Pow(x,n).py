2edb9e6d83710fca75b41cad66808f4406051c19
Bauyrzhan Zhakanov <36822661+BZWayne@users.noreply.github.com>
2019-06-23 19:40:04 +0600

    Create Pow(x,n).py

---
+++ Pow(x,n).py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def myPow(self, x, n):
+        if not n:
+            return 1
+        
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        
+        if n % 2:
+            return x * self.myPow(x, n-1)
+        
+        return self.myPow(x*x, n/2)
