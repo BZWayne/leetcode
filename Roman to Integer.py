d5d2ec017cedfd62c9faa67bd585cc654f5914de
Bauyrzhan Zhakanov <36822661+BZWayne@users.noreply.github.com>
2019-06-23 18:59:18 +0600

    Update Roman to Integer.py

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -1,1 +1,23 @@
-
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+        
+        chart = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        
+        ans = 0
+        for i in xrange(len(s) - 1):
+            if chart[s[i]] >= chart[s[i + 1]]:
+                ans += chart[s[i]]
+            else:
+                ans -= chart[s[i]]
+        
+        ans += chart[s[-1]]
+        
+        return ans
+        
+        
