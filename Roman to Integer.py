8fd6ff0d4e039c5278e39bcc24086efba21f8ea4
Bauyrzhan Zhakanov <36822661+BZWayne@users.noreply.github.com>
2019-06-23 19:00:04 +0600

    Update Roman to Integer.py

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -7,16 +7,16 @@ class Solution(object):
         if not s:
             return 0
         
-        chart = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        dict = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
         
         ans = 0
         for i in xrange(len(s) - 1):
-            if chart[s[i]] >= chart[s[i + 1]]:
-                ans += chart[s[i]]
+            if dict[s[i]] >= dict[s[i + 1]]:
+                ans += dict[s[i]]
             else:
-                ans -= chart[s[i]]
+                ans -= dict[s[i]]
         
-        ans += chart[s[-1]]
+        ans += dict[s[-1]]
         
         return ans
         
